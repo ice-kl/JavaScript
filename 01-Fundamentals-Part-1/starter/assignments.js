bc75fc17633fedae1534c8aaa1745e443505c9b3
Kantika Laking <kantika.l@haupcar.com>
2023-09-21 17:43:41 +0700

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,20 @@
+let country = "Thailand";
+const continent = "Asia";
+let population = 70;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = false;
+let language
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = "Thai";
+
+
+
