a165c9d9842cd8a85462f8bcbb89f0f416cd4822
ice-kl <123858675+ice-kl@users.noreply.github.com>
2023-09-22 17:20:46 +0700

    Add files via upload

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,20 +1,57 @@
+// Value and Variables
 let country = "Thailand";
 const continent = "Asia";
-let population = 70;
+let populationTh = 70;
 
 console.log(country);
 console.log(continent);
-console.log(population);
+console.log(populationTh);
 
+// Data Types
 let isIsland = false;
 let language
 
 console.log(typeof isIsland);
-console.log(typeof population);
+console.log(typeof populationTh);
 console.log(typeof country);
 console.log(typeof language);
 
 language = "Thai";
 
+// Basic operators
+console.log(populationTh / 2);
+
+populationTh++;
+console.log(populationTh);
+
+const populationFin = 6;
+console.log(populationTh > populationFin);
+
+console.log(populationTh < 33);
+
+let description = 
+    country + 
+    ' is in ' + 
+    continent + 
+    ', and its ' + 
+    populationTh + 
+    ' million people speak ' + 
+    language;
+console.log(description);
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark ** 2);
+const BMIJohn = massJohn / (heightJohn ** 2);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn);
+console.log(markHigherBMI);
+
+description = `${country} is in ${continent}, and its ${populationTh} million people speak ${language}`;
+console.log(description);
 
 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,57 @@
-let js = 'amazing';
-console.log(40+8+23-10);
+// let js = 'amazing';
+// console.log(40+8+23-10);
 
-let firstName = "Joody";
-console.log(firstName);
+// let firstName = "Joody";
+// console.log(firstName);
+
+// Math operators
+// const now = 2023;
+// const ageJoody = now - 1995;
+// const ageSarah = now - 2000;
+// console.log(ageJoody, ageSarah);
+
+// console.log(ageJoody * 2, ageJoody / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Joody";
+// const lastName = "Aaaaa";
+// console.log(firstName + " " + lastName);
+
+// // Assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+
+// // Comparison operators (return Boolean)
+// console.log(ageJoody > ageSarah); // > , < , >= , <=
+// console.log(ageJoody <= 13);
+
+// const isFullAge = ageJoody >= 13;
+
+// console.log(now - 1991 > now - 2018);
+
+const firstName = 'Joody';
+const job = 'teacher';
+const birthYear = 1995;
+const year = 2023;
+
+// Old template - string
+const joody = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(joody);
+
+// Template literal - string
+const joodyNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}! `;
+console.log(joodyNew);
+
+// Old template - new line
+console.log('Staring with \n\
+multiple \n\
+lines');
+
+// Template literal - new line
+console.log(`Staring with
+multiple
+lines`);
