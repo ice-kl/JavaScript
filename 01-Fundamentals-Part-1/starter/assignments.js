94460e2dc7cbad10c5ad18b7ec1dbf1e0007edf1
Kantika Laking <kantika@Kantikas-MacBook-Air.local>
2023-09-25 23:52:40 +0700

    finished part 1

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,57 +1,126 @@
-// Value and Variables
-let country = "Thailand";
-const continent = "Asia";
-let populationTh = 70;
+// // Value and Variables
+// const country = "Thailand";
+// const continent = "Asia";
+// const populationTh = 70;
 
-console.log(country);
-console.log(continent);
-console.log(populationTh);
+// console.log(country);
+// console.log(continent);
+// console.log(populationTh);
 
-// Data Types
-let isIsland = false;
-let language
+// // Data Types
+// let isIsland = false;
+// let language
 
-console.log(typeof isIsland);
-console.log(typeof populationTh);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof populationTh);
+// console.log(typeof country);
+// console.log(typeof language);
 
-language = "Thai";
+// language = "Thai";
 
-// Basic operators
-console.log(populationTh / 2);
+// // Basic operators
+// console.log(populationTh / 2);
 
-populationTh++;
-console.log(populationTh);
+// populationTh++;
+// console.log(populationTh);
 
-const populationFin = 6;
-console.log(populationTh > populationFin);
+// const populationFin = 6;
+// console.log(populationTh > populationFin);
 
-console.log(populationTh < 33);
+// console.log(populationTh < 33);
 
-let description = 
-    country + 
-    ' is in ' + 
-    continent + 
-    ', and its ' + 
-    populationTh + 
-    ' million people speak ' + 
-    language;
-console.log(description);
+// let description = 
+//     country + 
+//     ' is in ' + 
+//     continent + 
+//     ', and its ' + 
+//     populationTh + 
+//     ' million people speak ' + 
+//     language;
+// console.log(description);
 
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const BMIMark = massMark / (heightMark ** 2);
-const BMIJohn = massJohn / (heightJohn ** 2);
-const markHigherBMI = BMIMark > BMIJohn;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / heightJohn ** 2;
+// const markHigherBMI = BMIMark > BMIJohn;
 
-console.log(BMIMark, BMIJohn);
-console.log(markHigherBMI);
+// console.log(BMIMark, BMIJohn);
+// console.log(markHigherBMI);
 
-description = `${country} is in ${continent}, and its ${populationTh} million people speak ${language}`;
-console.log(description);
+// description = `${country} is in ${continent}, and its ${populationTh} million people speak ${language}`;
+// console.log(description);
 
+// const country = "Thailand";
+// const populationTh = 130;
+// const populationAvg = 33;
+
+// if(populationTh >= populationAvg){
+//     console.log(`${country} population is above average`);
+// }else{
+//     const populationBelowAvg = populationAvg - populationTh;
+//     console.log(`${country} population is ${populationBelowAvg} below average`);
+// }
+
+
+// console.log('9' - '5');  // result = 4
+// console.log('19' - '13' + '17'); // result = 617
+// console.log('19' - '13' + 17); // result = 23
+// console.log('123' < 57); // result = false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // result = 1143 
+
+// const numNeighbours = prompt('How many neighbour countries does your country have?');
+
+// if (numNeighbours === 1){
+//     console.log('Only 1 border!');
+// } else if (numNeighbours > 1){
+//     console.log('More than 1 border');
+// } else {
+//     console.log('No borders');
+// }
+
+// // LECTURE: Logical Operators
+// const country = 'Thailand';
+// const language = 'english';
+// const population = 40;
+// const isIsland = false;
+
+// if(language === 'english' && population < 50 && !isIsland){
+//     console.log(`You should live in ${country} :)`)
+// }else{
+//     console.log(`${country} does not meet your criteria :(`);
+// }
+
+// LECTURE: The switch Statement
+// const language = 'english';
+
+// switch (language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log('MOST number of native speakers!');
+//         break;
+//     case 'spanish:':
+//         console.log('2nd place in number of native speakers');
+//         break;
+//     case 'english':
+//         console.log('3rd place');
+//         break;
+//     case 'hindi':
+//         console.log('Number 4');
+//         break;
+//     case 'arabic':
+//         console.log('5th most spoken language');
+//         break;
+//     default:
+//         console.log('Great language too :D');
+// }
+
+// LECTURE: The Conditional (Ternary) Operator
+const population = 34;
+const country = 'Thailand';
+
+population > 33 ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`);
 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -33,25 +33,151 @@
 
 // console.log(now - 1991 > now - 2018);
 
-const firstName = 'Joody';
-const job = 'teacher';
-const birthYear = 1995;
-const year = 2023;
+// const firstName = 'Joody';
+// const job = 'teacher';
+// const birthYear = 1995;
+// const year = 2023;
 
-// Old template - string
-const joody = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
-console.log(joody);
+// // Old template - string
+// const joody = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+// console.log(joody);
 
-// Template literal - string
-const joodyNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}! `;
-console.log(joodyNew);
+// // Template literal - string
+// const joodyNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}! `;
+// console.log(joodyNew);
 
-// Old template - new line
-console.log('Staring with \n\
-multiple \n\
-lines');
+// // Old template - new line
+// console.log('Staring with \n\
+// multiple \n\
+// lines');
 
-// Template literal - new line
-console.log(`Staring with
-multiple
-lines`);
+// // Template literal - new line
+// console.log(`Staring with
+// multiple
+// lines`);
+
+
+// const age = 13;
+// const isOldEnough = age >= 18;
+
+// if(isOldEnough){
+//     console.log('Joody can start driving license 🚗') // add emuji = cmd + ctrl + space
+// }else{
+//     const yearLeft = 18 - age;
+//     console.log(`Joody is too young. Wait for another ${yearLeft} years :)`)
+// }
+
+
+// const birthYear = 1995;
+
+// let century;
+// if(birthYear <= 2000){
+//     century = 20;
+// }else{
+//     century = 21;
+// }
+// console.log(century);
+
+// Type conversion
+// Convert string to number
+// const inputYear = '1991';
+// console.log(Number(inputYear));
+// console.log(Number(inputYear) + 18);
+
+// // Convert number to string
+// console.log(String(23), 23); //in console, 23(white = string) 23(purple = number)
+
+// // Type coercion (2 type)
+//  console.log('I am ' + 23 + ' years old') // 23(number) was converted to string because of + operator 
+//  console.log('23' + '10' + 3)  // result is 23103 because of + operator convert number to string 
+//  console.log('23' - '10' - 3)  // result is 10 because of - operator convert string to number
+//  console.log('23' * '10')  // result is 230 because of * operator convert string to number
+//  console.log('23' / '10') // result is 2.3 because of / operator convert string to number
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+// const money = 0;
+// if(money){
+//     console.log("Don't spend it all!") // 100 is true, 0 is false
+// }else{
+//     console.log("You should get a job!")
+// }
+
+// const age = 18;
+// if(age === 18) console.log("You just became an adult"); // === (exact value) is boolean type means (18 === 18) return true 
+
+// const favourite = Number(prompt("What's your favourite number?"));
+// console.log(favourite);
+// console.log(typeof favourite);
+
+// if(favourite === 23){
+//     console.log('Cool! 23 is amazing number!')
+// } else if(favourite === 7){
+//     console.log('Cool! 7 is amazing number!')
+// } else if(favourite === 9){
+//     console.log('Cool! 9 is amazing number!')
+// } else {
+//     console.log('Number is not 23 or 9 or 7')
+// }
+
+// if(favourite !== 23) console.log('Why not 23?')
+
+// const hasDriverLicense = true;
+// const hasGoodVision = false;
+
+// console.log(hasDriverLicense && hasGoodVision); // and
+// console.log(hasDriverLicense || hasGoodVision); // or
+// console.log(!hasDriverLicense); // not
+
+// if(hasDriverLicense && hasGoodVision){
+//     console.log('Joody is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+
+// const day = 'friday';
+
+// switch (day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code example');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend ;)');
+//         break;
+//     default:
+//         console.log('Not a valid day');
+// }
+
+// Ternary operator
+// Ex.1
+const age = 8;
+age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');  // condition ? console.log(if true) : (if false);
+
+// Ex.2
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+// Ex.3
+let drink2
+if (age >= 18) {
+    drink2 = 'wine 🍷';
+} else {
+    drink2 = 'water 💧';
+}
+console.log(drink2)
+
+// Ex.4
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
